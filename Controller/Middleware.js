2b995d1696caee6dc1e24b4a49af9cf35df1b089
pranavyemul76@gmail.com <you@example.com>
2022-06-09 16:37:22 +0530

    phone

--- Controller/Middleware.js
+++ Controller/Middleware.js
@@ -40,8 +40,7 @@ exports.ValidationPersonal = (req, res, next) => {
         req.body[checkinput[i]] === undefined ||
         req.body[checkinput[i]] === "" ||
         (/[^0-9]/.test(req.body[checkinput[i]]) &&
-          req.body[checkinput[i]].toString().length <= 10 &&
-          req.body[checkinput[i]].toString().length >= 1)
+          phone.toString().length != 10)
       ) {
         res.json({ messeage: ` provide valid ${checkinput[i]}` });
         break;
