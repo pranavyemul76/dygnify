1b067a5add4d17c9e5c8f6fc7716b67b1c11ccba
pranavyemul76@gmail.com <you@example.com>
2022-06-11 20:28:08 +0530

    host

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
-const port = process.env.PORT || 8080;
-const host = process.env.HOST || "localhost";
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 const cors = require("cors");
 const Route = require("./Router");
